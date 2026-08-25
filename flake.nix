629a2d23e1ec2379caab14bfb7917de59e20b2cc
Red <me@nospawnn.com>
2026-08-25 19:30:39 +0100

    init flake

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+
+      forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f nixpkgs.legacyPackages.${system});
+    in
+    {
+      packages = forAllSystems (pkgs: {
+        default = pkgs.writeShellApplication {
+          name = "run";
+          runtimeInputs = [ pkgs.uv ];
+          text = ''
+            exec uv run --with-requirements "$PWD/requirements.txt" "$PWD/main.py" "$@"
+          '';
+        };
+      });
+
+      apps = forAllSystems (pkgs: {
+        default = {
+          type = "app";
+          program = "${self.packages.${pkgs.stdenv.hostPlatform.system}.default}/bin/run";
+        };
+      });
+    };
+}

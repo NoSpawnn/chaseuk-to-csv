edbb8bde72614bb4568cf0d6f0b721e8a4e70469
Red <me@nospawnn.com>
2026-08-25 21:23:19 +0100

    multi file support and print to stdout by default

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+from contextlib import contextmanager, nullcontext
 from datetime import datetime
 from itertools import batched
 from pypdf import PdfReader
 
 import argparse
 import csv
+import sys
 
 PDF_HEADER_STR = "Date Transaction details Amount Balance"
 
@@ -15,73 +17,75 @@ parser = argparse.ArgumentParser(
     prog="chase-to-csv",
     description="Convert Chase UK PDF statement to hledger-importable CSV",
 )
-parser.add_argument("target_pdf")
-parser.add_argument("outcsv")
+parser.add_argument("-f", "--files", nargs="+", required=True)
+parser.add_argument("-o", "--out")
 parser.add_argument("--use-old-parsing", action="store_true")
 
 args = parser.parse_args()
-reader = PdfReader(args.target_pdf)
-pages = reader.pages
 
 transactions = []
-start = None
-done = False
-failed = False
-for page_no, page in enumerate(pages):
-    if done or failed:
-        break
+for f in args.files:
+    reader = PdfReader(f)
+    pages = reader.pages
 
-    lines = page.extract_text().splitlines()
-    start = lines.index(PDF_HEADER_STR)
-    if start == -1:
-        print(f"Didn't find header string on page {page_no + 1}", file=sys.stderr)
-        continue
+    start = None
+    done = False
+    failed = False
+    for page_no, page in enumerate(pages):
+        if done or failed:
+            break
 
-    # TODO: figure out when they changed this/auto-detect it.
-    #       it used to be that the opening -> closing bal
-    #       was at the end of the page
-    if args.use_old_parsing:
-        lines[:] = lines[
-            : next(
-                (
-                    i
-                    for i in range(len(lines) - 1, -1, -1)
-                    if "Account statement" in lines[i]
-                ),
-                len(lines),
-            )
-        ]
-
-    skip = 2 if page_no == 0 else 1
-
-    for group in batched(lines[start + skip :], TRANSACTION_LINES):
-        if group[0].startswith("Page"):
+        lines = page.extract_text().splitlines()
+        start = lines.index(PDF_HEADER_STR)
+        if start == -1:
+            print(f"Didn't find header string on page {page_no + 1}", file=sys.stderr)
             continue
-        if "Closing balance" in group[0]:
-            done = True
-            break
 
-        try:
-            datestr = group[0][:11]
-            date = datetime.strptime(datestr, DATE_FMT)
-        except ValueError:
-            print(f"failed to parse date from {datestr}")
-            done = True
-            failed = True
-            break
+        # TODO: figure out when they changed this/auto-detect it
+        #       it used to be that the opening -> closing bal
+        #       was at the end of the page
+        if args.use_old_parsing:
+            lines[:] = lines[
+                : next(
+                    (
+                        i
+                        for i in range(len(lines) - 1, -1, -1)
+                        if "Account statement" in lines[i]
+                    ),
+                    len(lines),
+                )
+            ]
 
-        desc = group[0][12:]
-        note = group[1]
-        amount = group[2].split(" ")[0].replace("£", "")
+        skip = 2 if page_no == 0 else 1
 
-        transactions.append(
-            {
-                "date": date.strftime("%Y-%m-%d"),
-                "desc": desc,
-                "note": note,
-                "amount": amount,
-            }
-        )
+        for group in batched(lines[start + skip :], TRANSACTION_LINES):
+            if group[0].startswith("Page"):
+                continue
+            if "Closing balance" in group[0]:
+                done = True
+                break
+
+            try:
+                datestr = group[0][:11]
+                date = datetime.strptime(datestr, DATE_FMT)
+            except ValueError:
+                print(f"failed to parse date from {datestr}")
+                done = True
+                failed = True
+                break
+
+            desc = group[0][12:]
+            note = group[1]
+            amount = group[2].split(" ")[0].replace("£", "")
+
+            transactions.append(
+                {
+                    "date": date.strftime("%Y-%m-%d"),
+                    "desc": desc,
+                    "note": note,
+                    "amount": amount,
+                }
+            )
 
 if start is None or start == -1:
     print("Didn't find a line matching '" + PDF_HEADER_STR + "'", file=sys.stderr)
@@ -90,8 +94,17 @@ if start is None or start == -1:
 if failed:
     exit(1)
 
-with open(args.outcsv, "w", newline="") as csvfile:
-    writer = csv.DictWriter(csvfile, fieldnames=CSV_HEADER_FIELDS)
+
+@contextmanager
+def output_stream(path):
+    if path:
+        with open(path, "a", newline="") as stream:
+            yield stream
+    else:
+        yield sys.stdout
+
+
+with output_stream(args.out) as stream:
+    writer = csv.DictWriter(stream, fieldnames=CSV_HEADER_FIELDS)
     writer.writeheader()
-    for t in transactions:
-        writer.writerow(t)
+    writer.writerows(transactions)
